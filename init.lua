2e9597a385a87f448a0e26b668ae35f27ecb88fd
Ognen Slavkovski <oslavko@protonmail.com>
2025-03-20 22:14:20 +1100

    installed plugins alpha, indent-blankline and reformatted telescope.lua file.

--- init.lua
+++ init.lua
@@ -25,4 +25,6 @@ require('lazy').setup {
     require 'plugins.autocompletion',
     require 'plugins.autoformatting',
     require 'plugins.gitsigns',
+    require 'plugins.alpha',
+    require 'plugins.indent-blankline',
 }
